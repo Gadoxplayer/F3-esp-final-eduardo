cb865ac0eb21c0ca5b4ac54fb9b2c392ef84b450
EduardoCampo <eduardocampo91@gmail.com>
2022-09-21 00:59:51 -0500

    Pagina4 - trying to configure the logic for the validation using yup, not working yet but not breaking the build

--- components/formPersonalData/formPersonalData.tsx
+++ components/formPersonalData/formPersonalData.tsx
@@ -2,12 +2,31 @@ import { Box, TextField } from "@mui/material";
 import { useForm, useFormContext } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import UserSchema from "schemas/userSchema";
 
 export const FormPersonalData = () => {
   const { register } = useFormContext();
 
+  //   const methods = useForm({
+  //     mode: "onBlur",
+  //     resolver: yupResolver(UserSchema),
+  //   });
+  //   console.log(errors);
+
+  const formOptions: any = {
+    mode: "onBlur",
+    resolver: yupResolver(UserSchema),
+  };
+
+  const { formState }: any = useForm(formOptions);
+  const { errors }: any = formState;
+
+  const handleBlur = (errors: any) => {
+    console.log(errors.name, "hola");
+  };
+
   return (
-    <>
+    <form onBlur={handleBlur}>
       <Box sx={{ width: "100%", p: 2 }}>
         <TextField
           {...register("name")}
@@ -15,9 +34,11 @@ export const FormPersonalData = () => {
           fullWidth
           id="name"
           label="Name"
-          name="Name"
+          name="name"
           autoComplete="Name"
+          className={`form-control ${errors.name ? "is-invalid" : ""}`}
         />
+        <div className="invalid-feedback">{errors.name?.message}</div>
       </Box>
       <Box sx={{ width: "100%", p: 2 }}>
         <TextField
@@ -41,6 +62,6 @@ export const FormPersonalData = () => {
           autoComplete="email"
         />
       </Box>
-    </>
+    </form>
   );
 };
